ae7984c20b487e95975b152d65e9ac9fcc3eeaf4
Nachshon Bar sela <93506881+bar-sela@users.noreply.github.com>
2023-05-12 03:49:48 +0300

    Add files via upload
    תקלה בפרדיקציה במעבר על הנקודה השנייה .

--- Addaline.py
+++ Addaline.py
@@ -1,27 +1,35 @@
 # Adaline neural network
 import numpy as np
+from sklearn.model_selection import KFold
 
 
-def Adaline(Input, Target, lr=0.2, stop=0.001):
-    weight = np.random.random(Input.shape[1])  ## משקולות לכל פרמטר אפשרי
-    bias = np.random.random(1)  ## b
-    Error = [stop + 1]
-    # check the stop condition for the network
-    while Error[-1] > stop:
-        eg = (Error[-1])
-        error = []
-        for i in range(Input.shape[0]):
-            Y_pred   = sum(weight * Input[i]) + bias
-            # Update the weight
-            for j in range(Input.shape[1]):
-                weight[j] = weight[j] + lr * (Target[i] - Y_pred) * Input[i][j]
+def Adaline(Input, Target, lr=0.02, stop=0.001):
+    kfold = KFold(n_splits=5, shuffle=True, random_state=42)
+    for train_index, val_index in kfold.split(Input):
+        X_train, X_val = Input[train_index], Input[val_index]
+        y_train, y_val = Target[train_index], Target[val_index]
 
-            # Update the bias
-            bias = bias + lr * (Target[i] - Y_pred)
+        weight = np.random.random(X_train.shape[1])  ## משקולות לכל פרמטר אפשרי
+        bias = np.random.random(1)  ## b
+        Error = [stop + 1]
+        # check the stop condition for the network
+        while Error[-1] > stop:
+            eg = (Error[-1])
+            error = []
+            for i in range(X_train.shape[0]):  ## מעבר על כל הדגימות
+                Y_pred   = sum(weight * X_train[i]) + bias
+                # Update the weight
+                for j in range(100):
+                    print(j)
+                    weight[j] = weight[j] + lr * (y_train[i] - Y_pred) * X_train[i][j]
+                    if j == 99 :
+                        s  = 2
+                # Update the bias
+                bias = bias + lr * (X_val[i] - Y_pred)
 
-            # Store squared error value
-            error.append((Target[i] - Y_pred) ** 2)
-        # Store sum of square errors
-        Error.append(sum(error))   ## בכל אפוק - אנחנו נסכום את הסכום של הטעיות שיצא לנו במעבר על כל דגימה באימון - אם הסכום הכולל יהיה קטן מערך כלשהו - נדע לעצור
-        print('Error :', Error[-1])
+                # Store squared error value
+                error.append((X_val[i] - Y_pred) ** 2)
+            # Store sum of square errors
+            Error.append(sum(error))   ## בכל אפוק - אנחנו נסכום את הסכום של הטעיות שיצא לנו במעבר על כל דגימה באימון - אם הסכום הכולל יהיה קטן מערך כלשהו - נדע לעצור
+            print('Error :', Error[-1])
     return weight, bias
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Import necessary libraries
 import numpy as np
 
+import Addaline
+import ast
 """
 # Input dataset
 x = np.array([[1.0, 1.0, 1.0],
@@ -18,7 +20,7 @@ print('Bias :', b)
 
 
 def Exclude_uncorrect_vectors(vetcor):
-    NumOfOnce = 17
+    NumOfOnce = 5
     if  len(vetcor) != 101 :
         return  False
     for i in range(0,len(vetcor)):
@@ -31,31 +33,56 @@ def Exclude_uncorrect_vectors(vetcor):
 
 def loop_and_extractGoodVectors():
     file_path = 'result.txt'
-    l = []
+    listOfVectors = []
+    listOfAllTags = []
     # Load the vector from the file as a string
     with open(file_path, 'r') as file:
         s = file.readline()
         counter = 0
         while(s ) :
-            vector = np.array(eval(s))
+            curString = s
+            sToList  = ast.literal_eval(s)  ### ממיר את הווקטור שנקרא כמחרוזת לרשימה בפייתון עם מספרים שלמים
             s = file.readline()
-            if Exclude_uncorrect_vectors(vector):
+            if not Exclude_uncorrect_vectors(sToList):
                  continue
-            l.append(vector)
+            listOfAllTags.append(float(curString[1]))
+            listOfVectors.append(sToList)
+    print(len(listOfVectors))
+    return listOfVectors,listOfAllTags
 
-    return l
+def DeleteFirstVariable(listOfLists):
+      for i in range(0, len(listOfLists)) :
+          listOfLists[i] = listOfLists[i][1:]
 if __name__ == "__main__":
     print("start Algorithem : BET and Lamed")
-    listOfGoodVec = loop_and_extractGoodVectors()
-    listOfBetAndMem = filter(lambda x:True if x[0] != 3 else False,listOfGoodVec )
-    print(list(listOfBetAndMem))
+    listOfGoodVec,listOfAllTags = loop_and_extractGoodVectors()
+
+    listOfBetAndMem = list(filter(lambda x:True if x[0] != 3 else False,listOfGoodVec ))
+    DeleteFirstVariable(listOfBetAndMem)
+    listOfBetAndMem =  np.array(listOfBetAndMem )
+    tagsBetAndMem =  list(filter(lambda x: True if x != 3 else False, listOfAllTags))
+    tagsBetAndMem= np.array(tagsBetAndMem)
+    Addaline.Adaline(listOfBetAndMem,tagsBetAndMem)
 
 
 
 
+    """
+    listOfBetAndLamed =  list(filter(lambda x:True if x[0] != 2 else False,listOfGoodVec ))
+    listOfBetAndLamed =  np.array(listOfBetAndLamed[1:] )
+    tagsBetAndLamed =  list(filter(lambda x: True if x != 2 else False, listOfAllTags))
+    tagsBetAndLamed = np.array(tagsBetAndLamed)
 
+    listOfMemAndLamed =  list(filter(lambda x:True if x[0] != 1 else False,listOfGoodVec ))
+    listOfMemAndLamed=  np.array(listOfBetAndMem[1:] )
+    tagsMemAndLamed =  list(filter(lambda x: True if x != 1 else False, listOfAllTags))
+    tagsMemAndLamed = np.array( tagsMemAndLamed )
 
+    print(len(listOfBetAndMem))
+    print(len(listOfMemAndLamed))
+    print(len(listOfBetAndLamed))
 
+"""
 
 
 
